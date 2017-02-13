400dc0903ed29b1dedcae9c22f91033f19c32ea3
Aaron Gustafson <aaron@easy-designs.net>
2017-02-13 11:52:54 -0500

    Filters are working well now

--- _plugins/jekyll-filter-by_tag.rb
+++ _plugins/jekyll-filter-by_tag.rb
@@ -1,18 +1,22 @@
 module Jekyll
   module TagFilter
     def by_tag(collection, tag)
+      new_collection = []
       if !collection.kind_of?(Array)
         raise TypeError, "You can only use this filter on an Array"
       end
-      collection.keep_if { |item|
-        if item['tags'].kind_of?(String) && item['tags'] == '*'
-          true
-        elsif  item['tags'].kind_of?(Array) && item['tags'].include?(tag)
-          true
-        else
-          false
+      if tag
+        collection.each do |item|
+          puts "looking for #{tag} in "
+          puts item['tags'].inspect
+          puts item['tags'].include?(tag)
+          if ( item['tags'].kind_of?(String) && item['tags'] == '*' ) ||
+             ( item['tags'].kind_of?(Array) && item['tags'].include?(tag) )
+            new_collection << item
+          end
         end
-      }
+      end
+      new_collection
     end
   end
 end
